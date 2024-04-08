1e6f8bf6372e61321acc83b4da225ab858cf8745
rplemos <rafael_plemos@hotmail.com>
2024-04-08 18:07:10 -0300

    Added reference file checking, cleaned code and added some visual prints

--- src/main.py
+++ src/main.py
@@ -2,41 +2,41 @@ import pdb_parser
 import superimposers
 import sysfunctions
 import contacts_fast
+import os
+import sys
 
 def main():
-    mode, pdb_files, ref_pdb, atoms_to_be_aligned, rmsd = sysfunctions.cl_parse()
+    mode, pdb_files, ref_pdb, atoms_to_be_aligned, rmsd, avd_cutoff = sysfunctions.cl_parse()
+    try:
+        if not os.path.exists(ref_pdb):
+            raise FileNotFoundError(f"Reference file not found: {ref_pdb}")
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1) # error exiting
+        
     if mode == "BioPython": 
         file_list = superimposers.biopython_superimpose(pdb_files, ref_pdb, atoms_to_be_aligned, rmsd)
     elif mode == "TMAlign":
         file_list = superimposers.tmalign_superimpose(pdb_files, ref_pdb, rmsd)
-    
-    parsed_proteins = pdb_parser.parse_pdb(file_list)
 
-    distances = contacts_fast.fast_contacts(parsed_proteins[0], parsed_proteins[1])
-    contacts_fast.show_contacts(distances)
+    parsed_proteins = pdb_parser.parse_pdb(file_list)
     
-    # for protein in parsed_proteins:
-    #     for chain in protein.get_chains():
-    #         for residue in chain.get_residues():
-    #             for atom1 in residue.get_atoms():
-    #                 for atom2 in residue.get_atoms():
-    #                     if atom1 != atom2:
-    #                         print(atom1.atomname, atom2.atomname)
-    #                 #contacts.euclid(atom)
-    #                 #info_list = [protein.id, chain.id, residue.resnum, residue.resname, atom.atomname, atom.x, atom.y, atom.z]
-    #                 #print(info_list)
-    #     print(protein.id, protein.title)
-    
-    # a = {}
-    
-    # for protein in parsed_proteins:
-    #     for chain in protein.get_chains():
-    #         for residue in chain.get_residues():
-    #             for atom in residue.get_atoms():
-    #                 a[f"{residue.resname}:{atom.atomname}"] = [atom.x, atom.y, atom.z]
-                    
-    # for key,value in a.items():
-    #     print(key, value)            
-    
+    ref_distance = None
+    ref_protein = parsed_proteins[0].id
+    for protein in parsed_proteins:
+        print(f"Detecting contacts for {protein.id} against {protein.id}")
+        distances = contacts_fast.fast_contacts(protein, protein)
+        contacts_fast.show_contacts(distances)
+        if ref_distance is None:
+            ref_distance = distances
+        else:
+            avd, average_avd, contact_matches = contacts_fast.avd(ref_distance, distances, avd_cutoff)
+            if avd is not None:
+                print(f"Average AVD for {ref_protein} and {protein.id}: {average_avd}\nNumber of contact matches found: {contact_matches}\n")
+            else:
+                print(f"No contact matches found between {ref_protein} and {protein.id}.\nTry increasing the cutoff value.\n")     
+        print("-------------------------------------\n")
+  
+
 if __name__ == "__main__":
     main()
