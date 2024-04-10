5bf3120ed5b86a5776e41ce4c03ea6d768d8e3c8
rplemos <rafael_plemos@hotmail.com>
2024-04-10 15:28:21 -0300

    Minor changes

--- src/superimposers.py
+++ src/superimposers.py
@@ -32,7 +32,7 @@ def tmalign_superimpose(pdb_files, ref_pdb, rmsd):
     
     ref_name = os.path.basename(ref_pdb)    
     outputs = [ref_pdb] # starts the output with the reference (samples will be added after their processing)
-    output_dir = "./src/tmalignoutputs"
+    output_dir = "tmalignoutputs"
     
     if not os.path.exists(output_dir): # creates output folder if it doesn't exist
         os.mkdir(output_dir)
@@ -59,7 +59,7 @@ def tmalign_superimpose(pdb_files, ref_pdb, rmsd):
                 os.remove(file)
         else: # RMSD is lower than the cutoff
             outputs.append(f"{output_name}_rotate.pdb")
-            print(f"RMSD value for file '{sample_name}': {rmsd_value[0]}.\t PDB Files created!")
+            print(f"RMSD value for file '{sample_name}': {rmsd_value[0]}.\t PDB File '{output_name}_rotate.pdb' created!")
             files_to_delete = glob.glob(os.path.join(output_name + '*'))
             # keeps only the .pdb files
             files_to_delete = [file for file in files_to_delete if not file.endswith(".pdb")]
