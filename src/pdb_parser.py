7b334b9d99c2b88b4134d6f04c1c50a3b6f9708f
rplemos <rafael_plemos@hotmail.com>
2024-04-26 20:44:49 -0300

    Extended 'stacking' dictionary to all atoms, added occupancy check

--- src/pdb_parser.py
+++ src/pdb_parser.py
@@ -4,11 +4,12 @@ from numpy.linalg import svd
   
 
 stacking = {
-    'PHE':[11, 'CG','CZ'],
-    'TYR':[12, 'CG','CZ'],
-    'TRP':[14, 'CD2','CE2'],
-    'HIS':[10, 'CG','ND1','CE1','NE2','CD2']
+    'HIS':[10, 'CG','ND1','CE1','NE2','CD2'],
+    'PHE':[11, 'CG','CD1','CE1','CZ','CE2','CD2'],
+    'TRP':[14, 'CG','CD1','NE1','CE2','CZ2','CH2','CZ3','CE3','CD2'],
+    'TYR':[12, 'CG','CD1','CE1','CZ','CE2','CD2'],
 }
+
 
 def parse_pdb(pdb_files):
     """
@@ -33,8 +34,10 @@ def parse_pdb(pdb_files):
 
                 if line.startswith("HEADER"):
                     current_protein.id = line[62:]
+                    
                 elif line.startswith("TITLE"):
                     current_protein.set_title(line[10:])
+                    
                 elif line.startswith("ATOM"):
                     chain_id = line[21]
                     resnum = int(line[22:26])
@@ -61,14 +64,19 @@ def parse_pdb(pdb_files):
                     
                     x, y, z = float(line[30:38]), float(line[38:46]), float(line[46:54])
                     occupancy = float(line[55:60])
-    
-                    atom = Atom(atomname, x, y, z, occupancy, current_residue)
                     
-                    current_residue.atoms.append(atom)
+                    if occupancy > 0.3: # ignores low quality atoms (arbitrary value!)
+                        atom = Atom(atomname, x, y, z, occupancy, current_residue) # creates atom
+                        current_residue.atoms.append(atom)
+                    else:
+                        print(atomname, occupancy, current_residue.resname, current_residue.resnum, current_chain.id, current_protein.id)
                     
+                    # CHECKING FOR AROMATICS
                     if current_residue.resname in stacking:
                         allowed = stacking[current_residue.resname][1:]
                         all_atoms_have_occupancy_one = all(atom.occupancy == 1 for atom in current_residue.atoms if atom.atomname in allowed)
+                        
+                        # if ring has only one conformation and the residue is complete (all atoms populated)
                         if all_atoms_have_occupancy_one and len(current_residue.atoms) == stacking[current_residue.resname][0]:
                             ring_atoms = array([[atom.x, atom.y, atom.z] for atom in current_residue.atoms[5:]]) # ignores [N, CA, C, O] and [RNG] atoms
                             centroid_atom = centroid(current_residue, ring_atoms)
@@ -83,12 +91,16 @@ def parse_pdb(pdb_files):
                     current_protein = None
                     current_chain = None
                     current_residue = None
+                    
     return proteins
+
 
 def centroid(residue, ring_atoms):
     centroid = mean(ring_atoms, axis = 0)
     centroid_atom = Atom("RNG", centroid[0], centroid[1], centroid[2], 1, residue)
+    
     return centroid_atom
+
 
 def calc_normal_vector(ring_atoms):
     centroid = mean(ring_atoms, axis = 0) # axis=0 -> mean through the columns
