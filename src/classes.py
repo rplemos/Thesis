e3a6a910301575f4616c6c36903557682773af52
rplemos <rafael_plemos@hotmail.com>
2024-05-07 18:12:31 -0300

    Cleaned classes, added a few format parsing functions

--- src/classes.py
+++ src/classes.py
@@ -23,12 +23,15 @@ class Protein:
         for residue in self.get_residues():
             for atom in residue.atoms:
                 yield atom
+                
+    def count_residues(self):
+        return sum(1 for _ in self.get_residues())
 
 
 class Chain:
-    def __init__(self):
-        self.id = None
-        self.residues = []
+    def __init__(self, id, residues):
+        self.id = id
+        self.residues = residues
 
     def get_residues(self):
         for residue in self.residues:
@@ -36,21 +39,20 @@ class Chain:
 
 
 class Residue:
-    def __init__(self):
-        self.resnum = None
-        self.resname = None
-        self.atoms = []
-        self.chain = None
-        self.ring = False
-        self.normal_vector = None
+    def __init__(self, resnum, resname, atoms, chain, ring, normal_vector):
+        self.resnum = resnum
+        self.resname = resname
+        self.atoms = atoms
+        self.chain = chain
+        self.ring = ring
+        self.normal_vector = normal_vector
 
     def get_atoms(self):
         for atom in self.atoms:
             yield atom
-
         
 class Atom:
-    def __init__(self, atomname=None, x=None, y=None, z=None, occupancy=1, residue=None):
+    def __init__(self, atomname, x, y, z, occupancy, residue):
         self.atomname = atomname
         self.x = x
         self.y = y
@@ -73,17 +75,23 @@ class Contact:
     def to_list(self): # all info besides the atom objects
         return [self.idchain1, self.residueatom1, self.idchain2, self.residueatom2,
             self.distance, self.type]
+    
+    def to_contact(self):
+        return [self.idchain1.split(":")[1], self.residueatom1.split(":")[0], self.idchain2.split(":")[1], 
+                self.residueatom2.split(":")[0], self.type]
         
 
 class Match:
-    def __init__(self, avd, contact1, contact2):
+    def __init__(self, avd, contact1, contact2, d3d4):
         self.avd = avd
         self.contact1 = contact1
         self.contact2 = contact2
+        self.d3d4 = d3d4 
         
     def __eq__(self, other): # compares a Match object with another Match object
         if isinstance(other, Match): # checks if other is a Match object
             return (self.avd == other.avd and
                     self.contact1 == other.contact1 and
-                    self.contact2 == other.contact2)
+                    self.contact2 == other.contact2 and
+                    self.d3d4 == other.d3d4)
         return False
