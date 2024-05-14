b33ae89e943087d85dba6b5fc91a148fbcf1f18c
rplemos <rafael_plemos@hotmail.com>
2024-05-14 18:49:57 -0300

    Added new return functions, cleaned some code

--- src/classes.py
+++ src/classes.py
@@ -36,6 +36,10 @@ class Chain:
     def get_residues(self):
         for residue in self.residues:
             yield residue
+    
+    def count_residues(self):
+        return self.residues[-1].resnum
+        #return sum(1 for _ in self.get_residues())
 
 
 class Residue:
@@ -61,24 +65,47 @@ class Atom:
         self.residue = residue
 
 
-class Contact:
-    def __init__(self, idchain1, residueatom1, idchain2, residueatom2, distance, type, atom1, atom2):
-        self.idchain1 = idchain1
-        self.residueatom1 = residueatom1
-        self.idchain2 = idchain2
-        self.residueatom2 = residueatom2
+class Contact: 
+    def __init__(self, id1, chain1, residue_num1, residue_name1, atom1, 
+                 id2, chain2, residue_num2, residue_name2, atom2, 
+                 distance, type, atom_object1, atom_object2):
+        self.id1 = id1
+        self.chain1 = chain1
+        self.residue_num1 = residue_num1
+        self.residue_name1 = residue_name1
+        self.atom1 = atom1
+        self.id2 = id2
+        self.chain2 = chain2
+        self.residue_num2 = residue_num2
+        self.residue_name2 = residue_name2
+        self.atom2 = atom2
         self.distance = distance
         self.type = type
-        self.atom1 = atom1
-        self.atom2 = atom2
-
-    def to_list(self): # all info besides the atom objects
-        return [self.idchain1, self.residueatom1, self.idchain2, self.residueatom2,
-            self.distance, self.type]
+        self.atom_object1 = atom_object1
+        self.atom_object2 = atom_object2
+        
+    def get_values(self):
+        all_values = list(self.__dict__.values())
+        return all_values[:-2]
     
-    def to_contact(self):
-        return [self.idchain1.split(":")[1], self.residueatom1.split(":")[0], self.idchain2.split(":")[1], 
-                self.residueatom2.split(":")[0], self.type]
+    def print_values(self):
+        all_values = list(self.__dict__.values())
+        return [f"{all_values[0]}:{all_values[1]}", f"{all_values[2]}{all_values[3]}:{all_values[4]}",
+                f"{all_values[5]}:{all_values[6]}", f"{all_values[7]}{all_values[8]}:{all_values[9]}",
+                all_values[10], all_values[11]]
+    
+    def print_contact(self):
+        all_values = list(self.__dict__.values())
+        return f"Distance between {all_values[0]}:{all_values[1]}-{all_values[2]}{all_values[3]}:{all_values[4]} and {all_values[5]}:{all_values[6]}-{all_values[7]}{all_values[8]}:{all_values[9]}: {all_values[10]} A. Types: {all_values[11]}"
+    
+    # def to_contact2(self):
+    #     type = str(self.type)[2:-2].replace("'", "")
+    #     return [self.idchain1.split(":")[1], self.residueatom1.split(":")[0], self.idchain2.split(":")[1], 
+    #             self.residueatom2.split(":")[0], [f"{type} ({self.atom1.atomname}:{self.atom2.atomname})"]]
+        
+    # def to_contact(self):
+    #     return [self.idchain1.split(":")[1], self.residueatom1.split(":")[0], self.idchain2.split(":")[1], 
+    #             self.residueatom2.split(":")[0], self.type]
         
 
 class Match:
@@ -88,10 +115,10 @@ class Match:
         self.contact2 = contact2
         self.d3d4 = d3d4 
         
-    def __eq__(self, other): # compares a Match object with another Match object
-        if isinstance(other, Match): # checks if other is a Match object
-            return (self.avd == other.avd and
-                    self.contact1 == other.contact1 and
-                    self.contact2 == other.contact2 and
-                    self.d3d4 == other.d3d4)
-        return False
+    # def __eq__(self, other): # compares a Match object with another Match object
+    #     if isinstance(other, Match): # checks if other is a Match object
+    #         return (self.avd == other.avd and
+    #                 self.contact1 == other.contact1 and
+    #                 self.contact2 == other.contact2 and
+    #                 self.d3d4 == other.d3d4)
+    #     return False
