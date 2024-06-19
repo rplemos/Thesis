9ddcb178f5305d3efa3f37878a7dc5cfa267159d
rplemos <rafael_plemos@hotmail.com>
2024-06-19 17:55:15 -0300

    Cleaned code, added distances cutoffs to accelerate processing

--- src/contacts_fast.py
+++ src/contacts_fast.py
@@ -4,63 +4,60 @@ from numpy import dot, arccos, degrees
 from numpy.linalg import norm
 from classes import Contact, Match
 import conditions
+import distances
 
-
-def fast_contacts(protein, fast):
+def fast_contacts(protein, fast, maximum_distances):
     start = timer()
     
     residues = list(protein.get_residues())
     contacts = []
-    
-    # FOR GETTING ALL THE CHAINS ON THE REFERENCE PROTEIN, AND SETTING TO COMPARE ONLY TO THEM
-    chains = [chain.id for chain in protein.chains]
-    print(f"Chains to be analyzed: {chains}")
-    print(f"Protein size:\n\tFull (includes gaps): {protein.full_count()[1]}\n\tTrue (number of residues in the PDB file): {protein.true_count()}\n")
-    
-    big_ones = ["ARG", "LYS", "GLU", "PHE"]
-    
+            
     for i, residue1 in enumerate(residues):
         for j, residue2 in enumerate(residues[i+1:], start=i+1):
             
-            if residue1.resnum == residue2.resnum:
+            if residue1.resnum == residue2.resnum and residue1.chain.id == residue2.chain.id: # ignores same residue
                 continue
              
-            if residue1.chain.id in chains and residue2.chain.id in chains:
-                ca1, ca2 = residue1.atoms[1], residue2.atoms[1] # alpha carbons
-                distance_ca = dist((ca1.x, ca1.y, ca1.z), (ca2.x, ca2.y, ca2.z))
-                if residue1.resname not in big_ones and residue2.resname not in big_ones and distance_ca > 13:
-                    continue
-                elif distance_ca > 20:
-                    continue # skips the current residue 2
-                
-                # CHECKING FOR AROMATIC STACKINGS
-                if residue1.ring and residue2.ring:
-                    ring1, ring2 = residue1.atoms[-1], residue2.atoms[-1] # RNG atoms
-                    distance = dist((ring1.x, ring1.y, ring1.z), (ring2.x, ring2.y, ring2.z))
-                    angle = calc_angle(residue1.normal_vector, residue2.normal_vector)
-                    if distance >= 2 and distance <= 5: # within aromatic stacking limits
-                        if (160 <= angle < 180) or (0 <= angle < 20):
-                            stack_type = "-parallel"
-                        elif (80 <= angle < 100):
-                            stack_type = "-perpendicular"
-                        else:
-                            stack_type = "-other"
+            ca1, ca2 = residue1.atoms[1], residue2.atoms[1] # alpha carbons
+            distance_ca = dist((ca1.x, ca1.y, ca1.z), (ca2.x, ca2.y, ca2.z))
+            
+            pair = tuple(sorted((residue1.resname, residue2.resname)))
+            if distance_ca > (distances.distances[pair] + 1):
+                continue              
+            
+            # if distance_ca > 20:
+            #     #pass
+            #     continue # skips the current residue 2
+            
+            # CHECKING FOR AROMATIC STACKINGS
+            if residue1.ring and residue2.ring:
+                ring1, ring2 = residue1.atoms[-1], residue2.atoms[-1] # RNG atoms
+                distance = dist((ring1.x, ring1.y, ring1.z), (ring2.x, ring2.y, ring2.z))
+                angle = calc_angle(residue1.normal_vector, residue2.normal_vector)
+                if distance >= 2 and distance <= 5: # within aromatic stacking limits
+                    if (160 <= angle < 180) or (0 <= angle < 20):
+                        stack_type = "-parallel"
+                    elif (80 <= angle < 100):
+                        stack_type = "-perpendicular"
+                    else:
+                        stack_type = "-other"
 
-                        contact = Contact(protein.id, residue1.chain.id, residue1.resnum, residue1.resname, ring1.atomname, 
-                                        protein.id, residue2.chain.id, residue2.resnum, residue2.resname, ring2.atomname, 
-                                        float(f"{distance:.2f}"), "stacking"+stack_type, ring1, ring2)
+                    contact = Contact(protein.id, residue1.chain.id, residue1.resnum, residue1.resname, ring1.atomname, 
+                                    protein.id, residue2.chain.id, residue2.resnum, residue2.resname, ring2.atomname, 
+                                    float(f"{distance:.2f}"), "stacking"+stack_type, ring1, ring2)
+                    
+                    contacts.append(contact)
+                    
+            for atom1 in residue1.atoms:
+                for atom2 in residue2.atoms:
+                    name1 = f"{atom1.residue.resname}:{atom1.atomname}" # matches the pattern from contacts dictionary
+                    name2 = f"{atom2.residue.resname}:{atom2.atomname}"
+                    
+                    if name1 in conditions.contact_types and name2 in conditions.contact_types: # excludes the RNG atom and any different other
                         
-                        contacts.append(contact)
-                        
-                for atom1 in residue1.atoms:
-                    for atom2 in residue2.atoms:
-                        name1 = f"{atom1.residue.resname}:{atom1.atomname}" # matches the pattern from contacts dictionary
-                        name2 = f"{atom2.residue.resname}:{atom2.atomname}"
-                        if name1 in conditions.contact_types and name2 in conditions.contact_types: # excludes the RNG atom and any different other
-                         
-                            if atom1.atomname != 'CA' and atom2.atomname != 'CA': # no need to calculate again for alpha carbons
-                                distance = dist((atom1.x, atom1.y, atom1.z), (atom2.x, atom2.y, atom2.z))
-                                
+                        #if atom1.atomname != 'CA' and atom2.atomname != 'CA': # no need to calculate again for alpha carbons
+                            distance = dist((atom1.x, atom1.y, atom1.z), (atom2.x, atom2.y, atom2.z))
+                            
                             if distance <= 6: # max distance for contacts
                                 contact_types = []
                                 for contact_type, distance_range in conditions.categories.items():
@@ -74,29 +71,33 @@ def fast_contacts(protein, fast):
                                     
                                     if distance_range[0] <= distance <= distance_range[1]: # fits the range
                                         if conditions.contact_conditions[contact_type](name1, name2): # fits the type of contact
-                                            # if distance_ca > 13 and residue1.resname == 'ARG' and residue2.resname == 'ARG': # checking the weird distant ones
-                                            #     print(distance, distance_ca, residue1.chain.id, residue1.resnum, name1, residue2.chain.id, residue2.resnum, name2)
                                             contact_types.append(contact_type)
 
-                                if contact_types:
-                                    
-                                    if residue1.chain.id != residue2.chain.id:
-                                        pass
-                                        #print(residue1.chain.id, residue1.resname, residue1.resnum, residue2.chain.id, residue2.resname, residue2.resnum)
-                                                              
+                                if contact_types:                                                                                                  
                                     contact = Contact(protein.id, residue1.chain.id, residue1.resnum, residue1.resname, atom1.atomname, 
                                                     protein.id, residue2.chain.id, residue2.resnum, residue2.resname, atom2.atomname, 
                                                     float(f"{distance:.2f}"), contact_types[0], atom1, atom2)
-                                                                        
+                                    
                                     contacts.append(contact)
-                                                                                        
-                        else: # for control over non-standard atom names
-                            pass
-                            #print(f"Unknown atom: {name1} or {name2}")      
+                                        
+                                    ####################
+                                    # BLOCK FOR CONSTRUCTING MAXIMUM DISTANCES LIST                   
+                                    # if (residue1.resname, residue2.resname) not in maximum_distances:
+                                    #     maximum_distances[residue1.resname, residue2.resname] = [float(f"{distance_ca:.2f}"), residue1.resnum, residue2.resnum, protein.id, atom1.atomname, atom2.atomname, residue1.chain.id, residue2.chain.id, contact_types[0]]
+                                    #     #print(f"Creating {residue1.resname, residue2.resname} : {distance_ca}")
+                                    # elif distance_ca > maximum_distances[residue1.resname, residue2.resname][0]:
+                                    #     #print(f"Changing {residue1.resname, residue2.resname} from {maximum_distances[residue1.resname, residue2.resname][0]} to {distance_ca}")
+                                    #     maximum_distances[residue1.resname, residue2.resname] = [float(f"{distance_ca:.2f}"), residue1.resnum, residue2.resnum, protein.id, atom1.atomname, atom2.atomname, residue1.chain.id, residue2.chain.id, contact_types[0]]
+                                    ####################
+                                                                                                            
+                    else: # for control over non-standard atom names
+                        pass
+                        #print(f"Unknown atom: {name1} or {name2}")      
+    
     end = timer()
-    print(f"Contact Detection - Time elapsed: {end - start}\n")
+    current_time = end - start
 
-    return contacts
+    return contacts, current_time
 
 
 def avd(contact_list_protein1, contact_list_protein2, cutoff):
@@ -145,7 +146,6 @@ def avd(contact_list_protein1, contact_list_protein2, cutoff):
 
             avd = min(((d1 + d2)/2),((d3 + d4)/2))
             
-            # if avd < cutoff and ('hydrophobic' not in contact1.type or 'hydrophobic' not in contact2.type): # means that it's a match
             if avd < cutoff:
                 #if ('hydrogen_bond' not in contact1.type or 'hydrogen_bond' not in contact2.type):
                     d3d4 = True if (d1+d2)/2 > (d3+d4)/2 else False
@@ -176,13 +176,11 @@ def show_contacts(contacts):
 
     for category, count in sorted_categories:
         print(f"\nNumber of {category} occurrences:", count)
-        if count <= 5:
+        if count <= 2:
             print(f"All entries for {category}:")
             for entry in contacts:
                 if entry.type == category:
                     print("\t",entry.print_text())
-
-    print(f"\nTotal number of contacts: {len(contacts)}\n")    
 
 def calc_angle(vector1, vector2):
     dot_product = dot(vector1, vector2)
