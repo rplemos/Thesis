d5f353af4a6dd76a7c8afb49b8c80156afc1b04f
rplemos <rafael_plemos@hotmail.com>
2024-06-24 14:37:31 -0300

    Changed files to be parsed on call, and not all at once

--- src/main.py
+++ src/main.py
@@ -5,7 +5,6 @@ import pdb_parser
 import superimposers
 import sysfunctions
 import contacts_fast
-
 import contact_map_plot
 
 def main():
@@ -13,9 +12,7 @@ def main():
     start = timer()
 
     mode, pdb_files, ref_pdb, atoms_to_be_aligned, rmsd, avd_cutoff, plot, fast, show_contacts = sysfunctions.cl_parse()
-    
-    print(show_contacts)
-    
+        
     try:
         if not os.path.exists(ref_pdb):
             raise FileNotFoundError(f"Reference file not found: {ref_pdb}")
@@ -31,29 +28,33 @@ def main():
         file_list = [ref_pdb]
     elif mode == "Benchmark":
         file_list = pdb_files
-        
-    parsed_proteins = pdb_parser.parse_pdb(file_list)
-    
+        file_list.insert(0, ref_pdb)
+            
     ref_contacts = None
-    ref_protein = parsed_proteins[0].id
-    
     maximum_distances = {}
     
-    for protein in parsed_proteins:
-        print(f"Detecting contacts for {protein.id}")
-        chains = [chain.id for chain in protein.chains]
+    for file in file_list: 
+        
+        if file.endswith('.pdb'):
+            parsed_protein = pdb_parser.parse_pdb(file)
+        elif file.endswith('.pdbx') or file.endswith('.cif'):
+            parsed_protein = pdb_parser.parse_pdbx(file)     
+    
+            
+        chains = [chain.id for chain in parsed_protein.chains]
+        print(f"Detecting contacts for {parsed_protein.id}")
         print(f"Number of chains: {len(chains)}")
         print(f"Chains to be analyzed: {chains}")
-        print(f"Protein size:\n\tFull (includes gaps): {protein.full_count()[1]}\n\tTrue (number of residues in the PDB file): {protein.true_count()}\n")
+        print(f"Protein size:\n\tFull (includes gaps): {parsed_protein.full_count()[1]}\n\tTrue (number of residues in the PDB file): {parsed_protein.true_count()}\n")
         
-        contacts, time = contacts_fast.fast_contacts(protein, fast, maximum_distances)
+        contacts, time = contacts_fast.fast_contacts(parsed_protein, fast, maximum_distances)
         print(f"Number of contacts: {len(contacts)}")
         print(f"Contact Detection - Time elapsed: {time}\n")
         
         if show_contacts:
             contacts_fast.show_contacts(contacts)
                 
-        chain_residues, total_size = protein.full_count()
+        chain_residues, total_size = parsed_protein.full_count()
         
         # don't run both at the same time (they return the same thing, the first just plots as well)
         if plot:
@@ -64,12 +65,13 @@ def main():
         if mode != "Benchmark":            
             if ref_contacts is None:
                 ref_contacts = contacts
+                ref_protein = parsed_protein.id                
             else:
                 match_list, average_avd, contact_matches = contacts_fast.avd(ref_contacts, contacts, avd_cutoff)
                 if match_list is not None:
-                    print(f"Average AVD for {ref_protein} and {protein.id} (old): {average_avd}\nNumber of contact matches found: {contact_matches}\n")
+                    print(f"Average AVD for {ref_protein} and {parsed_protein.id} (old): {average_avd}\nNumber of contact matches found: {contact_matches}\n")
                 else:
-                    print(f"No contact matches found between {ref_protein} and {protein.id}.\nTry increasing the cutoff value.\n")     
+                    print(f"No contact matches found between {ref_protein} and {parsed_protein.id}.\nTry increasing the cutoff value.\n")     
         print("-------------------------------------\n")
      
     # ascending_distances = sorted(maximum_distances.items(), key=lambda x:x[1][0]) 
@@ -97,6 +99,8 @@ def main():
     
     end = timer()
     print(f"Total time elapsed: {end - start}\n")
+    
+    print(maximum_distances)
 
 if __name__ == "__main__":
     main()
