9b622bab24e426c1a4c65b454ddb7e5eccf4e3c9
rplemos <rafael_plemos@hotmail.com>
2024-06-28 17:33:40 -0300

    Added regression curves and R2 calculation

--- src/scatterplot.py
+++ src/scatterplot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
+from sklearn.metrics import r2_score 
 
 file = sys.argv[1]
 
@@ -13,33 +15,51 @@ with open(file) as f:
         elif "True" in line:
             size = (line.split(":")[1][1:])
             proteins[id].append(size)
-        elif line.startswith("Number of contacts"):
-            contacts = int(line.split(":")[1][1:])
-            proteins[id].append(contacts)
         elif line.startswith("Contact"):
             time = float(line.split(":")[1][1:])
             proteins[id].append(f"{time:.4f}")  
+        # elif line.startswith("Number of contacts"):
+        #     contacts = int(line.split(":")[1][1:])
+        #     proteins[id].append(contacts)
 
 ids = list(proteins.keys())
 sizes = [float(entry[0]) for entry in proteins.values()]
-contacts = [float(entry[1]) for entry in proteins.values()]
-times = [float(entry[2]) for entry in proteins.values()]
+times = [float(entry[1]) for entry in proteins.values()]
+#contacts = [float(entry[1]) for entry in proteins.values()]
 
-# Create a scatter plot to visualize the relationship between contacts and times
+sizes = np.array(sizes)
+times = np.array(times)
+
+model_linear = np.poly1d(np.polyfit(sizes, times, 1)) 
+model_quadratic = np.poly1d(np.polyfit(sizes, times, 2))
+  
+polyline = np.linspace(min(sizes), max(sizes), 100) 
+
+linear_polyline = model_linear(polyline)
+quadratic_polyline = model_quadratic(polyline) 
+
+r2_linear = r2_score(times, model_linear(sizes))
+print(f"R2 linear: {r2_linear}")
+
+r2_quadratic = r2_score(times, model_quadratic(sizes))
+print(f"R2 quadratic: {r2_quadratic}")
+
 plt.figure(figsize=(12, 8))
-
-# Plot the data
 plt.scatter(sizes, times, color='blue', edgecolor='k', alpha=0.7)
-
-# Add labels and title
+plt.plot(polyline, linear_polyline, label=f'Linear Fit\n$R^2 = {r2_linear:.2f}$') 
+plt.plot(polyline, quadratic_polyline, label=f'Quadratic Fit\n$R^2 = {r2_quadratic:.2f}$')
 plt.xlabel('Protein Size', fontsize=14)
 plt.ylabel('Time (s)', fontsize=14)
 plt.title('Time vs Protein Size', fontsize=16)
 
 # Annotate each point with the protein ID
-for contact, time, id in zip(sizes, times, ids):
-    plt.annotate(id, (contact, time), textcoords="offset points", xytext=(0, 10), ha='center', fontsize=9)
+# for contact, time, id in zip(sizes, times, ids):
+#     plt.annotate(id, (contact, time), textcoords="offset points", xytext=(0, 10), ha='center', fontsize=9)
 
-# Show the plot
 plt.tight_layout()
+plt.legend()
 plt.show()
+
+# models
+print(model_linear)
+print(model_quadratic) 
